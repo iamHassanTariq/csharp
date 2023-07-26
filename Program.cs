55614f0628c27e72fefb67906d12dda0d6333145
iamHassanTariq <139846153+iamHassanTariq@users.noreply.github.com>
2023-07-26 09:45:57 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int firstnumber;
+            int secondnumber;            
+            Console.WriteLine("Please Enter the First Number");
+            firstnumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the Second Number");
+            secondnumber = Convert.ToInt32(Console.ReadLine());  
+          
+            Console.WriteLine("Result is "+(firstnumber+secondnumber));
+        }
+    }
+}
